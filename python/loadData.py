0688defec6c8caebfa34f2a5fbd82d262e27e3cc
Zach-Russell Russell <zacharyrussell01@gmail.com>
2024-04-27 01:21:08 +0000

    Finish project2?

--- python/loadData.py
+++ python/loadData.py
@@ -2,34 +2,71 @@ import os
 from db import *
 import json
 
-### Traverse through files
-
+#path to json files
 path = "../data"
 
+#successful documents inserted
 clean_successful = 0
+
+
+##Note: this isn't being used now, realized that orphaned documents are in the corrupt file, not in successfully loaded files
+#non corrupted documents unable to be inserted
 clean_failed = 0
+
+#corrupted files
 corrupted = 0
 
-for (root, dirs, file) in os.walk(path):
-    for f in file:
-        print(f)
-        #Insert into mongodb
-        # assuming you have defined a connection to your db and collection already:
-
+for (root, dirs, fileArr) in os.walk(path):
+    for f in fileArr:
 
         #Loading or Opening the json file
         with open(path + "/" + f) as file_descriptor:
-            file_data = json.load(file_descriptor)
-            
-        # Inserting the loaded data in the collection
-        # if JSON contains data more than one entry
-        # insert_many is used else insert_one is used
+
+            #attempt to load json data
+            try:
+                file_data = json.load(file_descriptor)
+
+            #couldn't load, it's corrupted
+            except json.JSONDecodeError as e:
+                print("Corrupt File::", e)
+                print("Corrupt FileName:", f)
+                corrupted += 1
+
+                #skip this iteration of the loop, don't want to insert corrupted data
+                continue
+
+
         if isinstance(file_data, list):
-            collection.insert_many(file_data)  
+            #attempt to insert many files
+            try:
+                collection.insert_many(file_data)
+
+                #successful insertion
+                clean_successful += len(file_data)
+            
+            #not corrupted, but couldn't insert
+            except Exception as e:
+                print("Failed Insertion::", e)
+                clean_failed += len(file_data)
+
         else:
-            collection.insert_one(file_data)
+            #attempt to insert one file
+            try:
+                collection.insert_one(file_data)
 
+                #successful insertion
+                clean_successful += len(file_data)
 
+            #not corrupted, but couldn't insert
+            except Exception as e:
+                print("Failed Insertion::", e)
+                clean_failed += len(file_data)
+
+#print number of documents inserted
+print("Successfully inserted documents:", clean_successful)
+
+#print number of corrupt files, can't count number of documents since wasn't able to be imported
+print("Corrupt files:", corrupted)
 
 
 
